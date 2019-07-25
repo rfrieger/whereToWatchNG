abff86d0c3cdcee07ac75287dfb79b33da4a783b
rfrieger <rfrieger@gmail.com>
2019-07-24 21:41:31 -0400

    before server.js change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,9 @@
 
 export const environment = {
   production: false,
-  apiurl: 'http://localhost:8080/api/'
+  // apiurl: 'http://localhost:8080/api/'
+  apiurl: 'https://wheretowatchsoccer.herokuapp.com/api/'
+
 };
 
 /*
